ebb44e5ad6ba78527ea05ce385363951043696b2
sbros12 <89467237+sbros12@users.noreply.github.com>
2023-01-29 23:32:16 +0900

    Update lys230129.py

--- lys230129.py
+++ lys230129.py
@@ -20,32 +20,19 @@ def rsi(ohlc: pandas.DataFrame, period: int = 14):
     return pandas.Series(100 - (100/(1 + RS)), name = "RSI")
 
 # coin list
-coinlist = ["KRW-KAVA", "KRW-MTL", "KRW-ZIL", "KRW-ETH", "KRW-EOS", "KRW-NEAR", "KRW-SBD", "KRW-BORA", "KRW-WAVES", "KRW-FLOW", "KRW-SRM", "KRW-AXS", "KRW-PLA", "KRW-XRP", "KRW-ETC"]  
-lower25 = [] 
-higher72 = []
-higher77 = [] 
+coinlist = ["KRW-APT", "KRW-BTC", "KRW-SAND", "KRW-MANA", "KRW-MTL", "KRW-PUNDIX", "KRW-ETH", "KRW-DOGE", "KRW-GMT", "KRW-BCH", "KRW-NEAR", "KRW-XTZ", "KRW-BTG", "KRW-BAT", "KRW-SBD", "KRW-ENJ", "KRW-SOL", "KRW-FLOW", "KRW-SRM", "KRW-AXS", "KRW-AVAX", "KRW-XRP", "KRW-ETC"]  
+lower1 = [] 
+lower2 = [] 
+lower3 = [] 
+higher1 = []
+higher2 = [] 
+higher3 = [] 
 
 # auto trade start
 upbit = pyupbit.Upbit(access, secret)
 print("Auto angel start")
 
 #get
-def buy1(coin): 
-    money = upbit.get_balance("KRW") 
-    amount = upbit.get_balance(coin) 
-    cur_price = pyupbit.get_current_price(coin) 
-    avg_price = upbit.get_avg_buy_price(coin)
-    total = amount * cur_price
-    rate_return = cur_price / (avg_price + 0.00001) * 100
-    h = 100000 # get unit 
-    if total <= 100 and money > 101000 : 
-        res = upbit.buy_market_order(coin, h) 
-    elif 100 < total < 1000000 and rate_return < 98 and money > 101000 : 
-        res = upbit.buy_market_order(coin, h) 
-    else :  
-        pass 
-    return
-
 def buy(coin): 
     money = upbit.get_balance("KRW") 
     amount = upbit.get_balance(coin) 
@@ -54,13 +41,16 @@ def buy(coin):
     total = amount * cur_price
     rate_return = cur_price / (avg_price + 0.00001) * 100
     h = 100000 # get unit 
-    if total <= 1000 and money > 101000 : 
+    if total <= 100 and money > 100100 : 
         res = upbit.buy_market_order(coin, h) 
-    elif 1000 < total < 1000000 and rate_return < 95 and money > 101000 : 
+    elif 100 < total < 1000000 and 96 < rate_return <= 99 and money > 100100 : 
         res = upbit.buy_market_order(coin, h) 
+    elif 100 < total < 1000000 and rate_return <= 96 and money > 100100 : 
+        res = upbit.buy_market_order(coin, h * 2)
     else :  
         pass 
     return
+
 
 #sell
 def sell(coin): 
@@ -79,6 +69,7 @@ def sell(coin):
         pass
     return
 
+
     # initiate 
 for i in range(len(coinlist)):
     smember.append(False)
@@ -96,15 +87,20 @@ while(True):
         print("RSI :", now_rsi) 
         print() 
         
-        if 35 <= now_rsi <= 68 :
+        if 36 <= now_rsi <= 66 :
             smember[i] = True 
             lower1[i] = False
             lower2[i] = False
+            lower3[i] = False
+            higher1[i] = False
+            higher2[i] = False
+            higher3[i] = False
+            
             
         elif 29 < now_rsi <= 32 : 
             lower1[i] = True 
         elif now_rsi >= 34 and lower1[i] == True and smember[i] == True:
-            buy1(coinlist[i]) 
+            buy(coinlist[i]) 
             smember[i] = False
             
         elif 23 < now_rsi <= 26 : 
@@ -115,23 +111,27 @@ while(True):
             
         elif now_rsi <= 18 : 
             lower3[i] = True   
-        elif now_rsi >= 20 and lower3[i] == True and smember[i] == True: 
+        elif now_rsi >= 20 and lower3[i] == True: 
             buy(coinlist[i]) 
             smember[i] = False
             
             
-        elif 72 <= now_rsi < 77 :
-            higher72[i] = True
-        elif now_rsi < 70 and higher72[i] == True: 
+        elif 70 <= now_rsi < 73 :
+            higher1[i] = True
+        elif now_rsi < 68 and higher1[i] == True and smember[i] == True: 
             sell(coinlist[i]) 
-            higher72[i] = False
-
-        elif now_rsi >= 77 :
-            higher77[i] = True
-                
-        elif now_rsi < 73 and higher77[i] == True: 
+            smember[i] = False
+            
+        elif 76 <= now_rsi < 79 :
+            higher2[i] = True
+        elif now_rsi < 74 and higher2[i] == True and smember[i] == True: 
             sell(coinlist[i]) 
-            higher77[i] = False
-            higher72[i] = False
+            smember[i] = False
+            
+        elif now_rsi >= 83 :
+            higher3[i] = True            
+        elif now_rsi < 81 and higher3[i] == True: 
+            sell(coinlist[i]) 
+            smember[i] = False
     
     time.sleep(5)
