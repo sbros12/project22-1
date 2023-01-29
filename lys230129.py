6bad88c490956152d90e3b5166f580051edc8c2a
sbros12 <89467237+sbros12@users.noreply.github.com>
2023-01-29 18:13:31 +0900

    Update lys230129.py

--- lys230129.py
+++ lys230129.py
@@ -30,6 +30,22 @@ upbit = pyupbit.Upbit(access, secret)
 print("Auto angel start")
 
 #get
+def buy1(coin): 
+    money = upbit.get_balance("KRW") 
+    amount = upbit.get_balance(coin) 
+    cur_price = pyupbit.get_current_price(coin) 
+    avg_price = upbit.get_avg_buy_price(coin)
+    total = amount * cur_price
+    rate_return = cur_price / (avg_price + 0.00001) * 100
+    h = 100000 # get unit 
+    if total <= 100 and money > 101000 : 
+        res = upbit.buy_market_order(coin, h) 
+    elif 100 < total < 1000000 and rate_return < 98 and money > 101000 : 
+        res = upbit.buy_market_order(coin, h) 
+    else :  
+        pass 
+    return
+
 def buy(coin): 
     money = upbit.get_balance("KRW") 
     amount = upbit.get_balance(coin) 
@@ -64,10 +80,12 @@ def sell(coin):
     return
 
     # initiate 
-for i in range(len(coinlist)): 
-    lower25.append(False) 
-    higher72.append(False)
-    higher77.append(False)
+for i in range(len(coinlist)):
+    smember.append(False)
+    lower1.append(False) 
+    lower2.append(False) 
+    higher1.append(False)
+    higher2.append(False)
 
 while(True): 
     for i in range(len(coinlist)): 
@@ -77,11 +95,30 @@ while(True):
         print("time: ", datetime.datetime.now()) 
         print("RSI :", now_rsi) 
         print() 
-        if now_rsi <= 25 : 
-            lower25[i] = True 
-        elif now_rsi >= 27 and lower25[i] == True: 
+        
+        if 35 <= now_rsi <= 68 :
+            smember[i] = True 
+            lower1[i] = False
+            lower2[i] = False
+            
+        elif 29 < now_rsi <= 32 : 
+            lower1[i] = True 
+        elif now_rsi >= 34 and lower1[i] == True and smember[i] == True:
+            buy1(coinlist[i]) 
+            smember[i] = False
+            
+        elif 23 < now_rsi <= 26 : 
+            lower2[i] = True   
+        elif now_rsi >= 28 and lower2[i] == True and smember[i] == True: 
             buy(coinlist[i]) 
-            lower25[i] = False
+            smember[i] = False
+            
+        elif now_rsi <= 18 : 
+            lower3[i] = True   
+        elif now_rsi >= 20 and lower3[i] == True and smember[i] == True: 
+            buy(coinlist[i]) 
+            smember[i] = False
+            
             
         elif 72 <= now_rsi < 77 :
             higher72[i] = True
